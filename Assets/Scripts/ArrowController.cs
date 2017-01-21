8ada211628c711ce275406be8af13e802cb3e246
kkk <karry9527100@gmail.com>
2017-01-21 20:00:25 +0800

    Add bouncy controller(only Ground01) Add vector arrow

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -2,34 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseDrag : MonoBehaviour {
-
+public class ArrowController : MonoBehaviour {
     private Vector2 initialPosition;
     private Vector2 offset;
     private Vector2 direction;
-    public float speed = 200.0f;
-    public GameObject arrow;
 
-    private Rigidbody2D rb;
-
-    void Start()
-    {
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        rb.isKinematic = true;
-    }
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+        transform.Rotate(0, 0, 90);*/
+        //transform.rotation +=Quaternion()
+	}
     void OnMouseDown()
     {
         initialPosition = new Vector2(Input.mousePosition.x / Camera.main.pixelWidth, Input.mousePosition.y / Camera.main.pixelHeight);
-        //Debug.Log(initialPosition);
+        Debug.Log(initialPosition);
     }
 
     void OnMouseDrag()
     {
         Vector2 currentPosition = new Vector2(Input.mousePosition.x / Camera.main.pixelWidth, Input.mousePosition.y / Camera.main.pixelHeight);
-        direction = currentPosition - initialPosition;
-
-        // Arrow control
         transform.position = new Vector3((currentPosition.x + initialPosition.x) / 2, (currentPosition.y + initialPosition.y) / 2, 0);
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
@@ -40,9 +38,7 @@ public class MouseDrag : MonoBehaviour {
     void OnMouseUp()
     {
 
-        direction = direction / direction.magnitude;    // Normalize 
-        rb.velocity = -direction * speed * 1000 * Time.deltaTime;
-        rb.isKinematic = false;
-        
+        //direction = direction / direction.magnitude;    // Normalize 
+        gameObject.SetActive(false);
     }
 }
