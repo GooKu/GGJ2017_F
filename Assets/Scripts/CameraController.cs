ce6a80f88d386c5fed3614dd7d619cf35498f85e
GooKu <kuowods@hotmail.com>
2017-01-21 11:37:36 +0800

    increase CameraController(interface)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public enum Mode
+    {
+        PlayerControl,
+        FollowTrager,
+        Stop
+    }
+
+    private Transform traget;
+
+    public void SetTraget(Transform traget)
+    {
+        this.traget = traget;
+    }
+
+    public void SetBoundary(Rect boundary)
+    {
+
+    }
+
+    public void UpdateMode(Mode mode)
+    {
+
+    }
+	
+	void Update () {
+		
+	}
+}
