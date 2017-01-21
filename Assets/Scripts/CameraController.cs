e86dbed586f1c8b0a464535d577f59af917ff890
GooKu <kuowods@hotmail.com>
2017-01-21 12:17:29 +0800

    Implement FollowTrager function and test scene setting.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,24 +10,47 @@ public class CameraController : MonoBehaviour {
         Stop
     }
 
-    private Transform traget;
+    private float distance = 10;
 
-    public void SetTraget(Transform traget)
+    private Transform traget;
+    private Rect boundary;
+    private Mode currentMode;
+    private float tragetZ;
+
+    public void Init(Transform traget, Rect boundary)
     {
         this.traget = traget;
-    }
-
-    public void SetBoundary(Rect boundary)
-    {
-
+        this.boundary = boundary;
+        tragetZ = traget.position.z - distance;
     }
 
     public void UpdateMode(Mode mode)
     {
-
+        currentMode = mode;
     }
 	
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        switch (currentMode)
+        {
+            case Mode.FollowTrager:
+                transform.position = Vector3.Lerp(transform.position, new Vector3(traget.position.x, traget.position.y, tragetZ), Time.deltaTime);
+                break;
+            case Mode.PlayerControl:
+                break;
+            case Mode.Stop:
+                break;
+        }
+
+        if (transform.position.x > boundary.xMax)
+            transform.position= new Vector3( boundary.xMax, transform.position.y, tragetZ) ;
+        else if (transform.position.x < boundary.xMin)
+            transform.position = new Vector3(boundary.xMin, transform.position.y, tragetZ);
+
+        if (transform.position.y > boundary.yMax)
+            transform.position = new Vector3(transform.position.x, boundary.yMax, tragetZ);
+        else if (transform.position.x < boundary.xMin)
+            transform.position = new Vector3(transform.position.x, boundary.yMin, tragetZ);
+
+    }
 }
--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class test : MonoBehaviour {
+    public Transform Traget;
+    public CameraController controller;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        controller.Init(Traget, new Rect(0, 0,20, 20));
+        controller.UpdateMode(CameraController.Mode.FollowTrager);
+    }
 	
 	// Update is called once per frame
 	void Update () {
