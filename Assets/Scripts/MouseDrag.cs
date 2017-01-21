f1231ac09393fdd12b18a25df02ce84b5a69b95f
Siyuan <easy0519@gmail.com>
2017-01-21 21:18:08 +0800

    移除 isPlaying 改用 enabled 控制, 加入 Fired event (for LevelController)

--- Assets/Scripts/MouseDrag.cs
+++ Assets/Scripts/MouseDrag.cs
@@ -8,9 +8,11 @@ public class MouseDrag : MonoBehaviour {
     private Vector2 offset;
     private Vector2 direction;
     private Vector3 initialWorldPosition;
-    private bool isPlaying;
+
     public float speed = 200.0f;
     public GameObject arrow;
+
+	public event System.EventHandler Fired;
 
     private Rigidbody2D rb;
 
@@ -18,19 +20,24 @@ public class MouseDrag : MonoBehaviour {
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         rb.isKinematic = true;
-        arrow.SetActive(false);
-        isPlaying = false;
+
+		if (arrow != null) {
+			arrow.SetActive (false);
+		}
     }
 
     void OnMouseDown()
     {
-        if (!isPlaying)
+		if (!this.enabled)
         {
             initialPosition = new Vector2(Input.mousePosition.x / Camera.main.pixelWidth, Input.mousePosition.y / Camera.main.pixelHeight);
             initialWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             initialWorldPosition.z = 0;
-            arrow.transform.localScale = Vector3.zero;
-            arrow.SetActive(true);
+
+			if (arrow != null) {
+				arrow.transform.localScale = Vector3.zero;
+				arrow.SetActive (true);
+			}
             //Debug.Log(initialPosition);
         }
     }
@@ -38,35 +45,38 @@ public class MouseDrag : MonoBehaviour {
     void OnMouseDrag()
     {
 
-        if (!isPlaying)
-        {
-            Vector2 currentPosition = new Vector2(Input.mousePosition.x / Camera.main.pixelWidth, Input.mousePosition.y / Camera.main.pixelHeight);
-            direction = currentPosition - initialPosition;
+		if (!this.enabled) {
+			Vector2 currentPosition = new Vector2 (Input.mousePosition.x / Camera.main.pixelWidth, Input.mousePosition.y / Camera.main.pixelHeight);
+			direction = currentPosition - initialPosition;
 
-            // Arrow control
-            Vector3 currentWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            currentWorldPosition.z = 0;
-            arrow.transform.position = new Vector3((currentWorldPosition.x + initialWorldPosition.x) / 2, (currentWorldPosition.y + initialWorldPosition.y) / 2, 0);
-            arrow.transform.rotation = Quaternion.LookRotation(Vector3.forward, currentWorldPosition - transform.position);
-            arrow.transform.Rotate(0, 0, -90);
-            arrow.transform.localScale = new Vector3(Vector3.Distance(currentWorldPosition, initialWorldPosition) / 200, transform.localScale.y, transform.localScale.z);
-        }
+			// Arrow control
+			Vector3 currentWorldPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			currentWorldPosition.z = 0;
+
+			if (arrow != null) {
+				arrow.transform.position = new Vector3 ((currentWorldPosition.x + initialWorldPosition.x) / 2, (currentWorldPosition.y + initialWorldPosition.y) / 2, 0);
+				arrow.transform.rotation = Quaternion.LookRotation (Vector3.forward, currentWorldPosition - transform.position);
+				arrow.transform.Rotate (0, 0, -90);
+				arrow.transform.localScale = new Vector3 (Vector3.Distance (currentWorldPosition, initialWorldPosition) / 200, transform.localScale.y, transform.localScale.z);
+			}
+		}
     }
 
     void OnMouseUp()
     {
-        if (!isPlaying)
+		if (!this.enabled)
         {
             direction = direction / direction.magnitude;    // Normalize 
             rb.velocity = -direction * speed * 1000 * Time.deltaTime;
             rb.isKinematic = false;
 
-            arrow.SetActive(false);
-        }
-    }
+			if (arrow != null) {
+				arrow.SetActive (false);
+			}
 
-    public bool IsPlaying()
-    {
-        return isPlaying;
+			if (Fired != null) {
+				Fired (this, System.EventArgs.Empty);
+			}
+        }
     }
 }
