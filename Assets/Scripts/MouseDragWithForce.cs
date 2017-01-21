13a5a0c3614edb2c5fb05be587faa618b7ade1f4
kkk <karry9527100@gmail.com>
2017-01-21 13:40:18 +0800

    Implement ball control. Include two types: 1. MouseDrag: Compute velocity only by angle. 2. MouseDragWithForce: Compute velocity by angle and distance.

---
+++ Assets/Scripts/MouseDragWithForce.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDragWithForce : MonoBehaviour
+{
+
+    private Vector2 initialPosition;
+    private Vector2 offset;
+    private Vector2 direction;
+    public float speed = 2.0f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void OnMouseDown()
+    {
+        initialPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        //Debug.Log(initialPosition);
+    }
+
+    void OnMouseDrag()
+    {
+        Vector2 currentPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        direction = currentPosition - initialPosition;
+        //Debug.Log(cursorPosition);
+    }
+
+    void OnMouseUp()
+    {
+
+        //direction = direction / direction.magnitude;    // Normalize 
+        rb.velocity = -direction * speed * Time.deltaTime;
+
+    }
+}
