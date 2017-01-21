23944ff0ed83e1cca1a6bfc47b7b4c3fc9308081
kkk <karry9527100@gmail.com>
2017-01-21 20:56:38 +0800

    Add the function to change trails' materials.

---
+++ Assets/Scripts/GravityMachine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityMachine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.tag == "Player")
+        {
+            float distance = Vector3.Distance(transform.position, collider.gameObject.transform.position);
+            Vector3 direction = Vector3.Normalize(transform.position - collider.gameObject.transform.position);
+
+            //collider.rigidbody2D.F
+        }
+    }
+}
---
+++ Assets/Scripts/TrailController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailController : MonoBehaviour {
+    enum Type
+    {
+        dayTime = 0,
+        Twilight = 1,
+        night = 2
+    }
+
+    public Material[] materials;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetMaterial(Type dayStatus)
+    {
+        GetComponent<TrailRenderer>().sharedMaterial = materials[(int)dayStatus];
+    }
+}
