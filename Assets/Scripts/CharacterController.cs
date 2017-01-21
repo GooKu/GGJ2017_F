5e4f28e8a782258d5a678109be9e527114b8b6d7
GooKu <kuowods@hotmail.com>
2017-01-21 22:05:29 +0800

    increase character controller.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public event System.EventHandler Fired;
+
+    public void SendFire()
+    {
+    }
+}
