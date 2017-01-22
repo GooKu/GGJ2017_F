9c01dcfeabc7897663fd356341f88ea66114cb67
kkk <karry9527100@gmail.com>
2017-01-22 14:55:37 +0800

    Add BGM

---
+++ Assets/Scripts/AudioFadeInOut.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFadeInOut : MonoBehaviour {
+    private AudioSource audio;
+    private float time;
+
+    public float threshold;
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if(time < threshold)
+        {
+            audio.volume = Mathf.Lerp(0.0f, 0.8f, time / threshold);
+        }
+        if(!audio.isPlaying)
+        {
+            time = 0;
+            audio.Play();
+        }
+	}
+}
