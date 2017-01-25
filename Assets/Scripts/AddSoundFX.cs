3a57597f9e678716c277c0fcaa36587ec1e0aafb
cathy1666 <cathy1666@hotmail.com.tw>
2017-01-25 21:25:13 +0800

    add asset SFX

---
+++ Assets/Scripts/AddSoundFX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSoundFX : MonoBehaviour {
+    public AudioClip myAuioClip;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D Coll)
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(myAuioClip);
+    }
+    void OnCollisionEnter2D(Collision2D Coll)
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(myAuioClip);
+    }
+}
